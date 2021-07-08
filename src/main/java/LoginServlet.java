fbe7d31f5e2fd3ce9d954eb1e1f82dc481fc3057
Benny Alvarez <benjamin.c.alvarez.21@gmail.com>
2021-07-08 14:44:48 -0500

    walkthrough

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,20 +3,30 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/login.jsp").forward(request, response);
+        if(request.getSession().getAttribute("user") != null) {
+            response.sendRedirect("/profile");
+            return;
+        }
+
+        request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         boolean validAttempt = username.equals("admin") && password.equals("password");
+        boolean isLoggedIn = username.equals("jaycodeup") && password.equals("test");
 
-        if (validAttempt) {
+        HttpSession session = request.getSession(); //session object we can set values
+
+        if (isLoggedIn) {
+            session.setAttribute("user", username);   //this is the key username and value
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
