0c4057bb495b555d38cfec12ff75da6790878c9a
Benny Alvarez <benjamin.c.alvarez.21@gmail.com>
2021-07-13 11:56:42 -0500

    completed exercise and walkthrough for userinput

--- src/main/java/com/codeup/adlister/dao/DaoFactory.java
+++ src/main/java/com/codeup/adlister/dao/DaoFactory.java
@@ -1,7 +1,9 @@
+
 package com.codeup.adlister.dao;
 
 public class DaoFactory {
     private static Ads adsDao;
+    private static Users usersDao;
     private static Config config = new Config();
 
     public static Ads getAdsDao() {
@@ -10,4 +12,34 @@ public class DaoFactory {
         }
         return adsDao;
     }
+
+    public static Users getUsersDao() {
+        if (usersDao == null) {
+            usersDao = new MySQLUsersDao(config);
+        }
+        return usersDao;
+    }
 }
+
+//package com.codeup.adlister.dao;
+//
+//public class DaoFactory {
+//    private static Ads adsDao;
+//    private static Users usersDao;
+//    private static Config config = new Config();
+//    private static Users userDao;
+//
+//    public static Ads getAdsDao() {
+//        if (adsDao == null) {
+//            adsDao = new MySQLAdsDao(config);
+//        }
+//        return adsDao;
+//    }
+//
+//    public static Users getUserDao() {
+//        if (userDao == null) {
+//            userDao = (Users) new MySQLUsersDao(config);
+//        }
+//        return userDao;
+//    }
+//}
